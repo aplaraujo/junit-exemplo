112d2b5fe5b713f13d30d963cd5f494747e06ca4
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-10-02 16:11:48 -0300

    Deposit test

---
+++ src/tests/entities/AccountTest.java
@@ -0,0 +1,19 @@
+package tests.entities;
+
+import entities.Account;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AccountTest {
+    @Test
+    public void depositShouldIncreaseBalanceWhenPositiveAmount() {
+        // Arrange
+        double amount = 200.00;
+        double expectedValue = 196.00;
+        Account acc = new Account(0.00);
+        // Act
+        acc.deposit(amount);
+        // Assert
+        Assertions.assertEquals(expectedValue, acc.getBalance());
+    }
+}
